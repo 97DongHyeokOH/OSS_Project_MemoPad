e003bd29d6bc379af982e521c2bb8c997f8ba8b4
DongHyeok OH <donggar97@naver.com>
2020-06-27 14:25:15 +0900

    추가/삭제 버튼 추가

--- app/src/main/java/com/professionalandroid/oss_project_memopad/MainActivity.java
+++ app/src/main/java/com/professionalandroid/oss_project_memopad/MainActivity.java
@@ -1,13 +1,22 @@
 package com.professionalandroid.oss_project_memopad;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button add;
+    private Button delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.add);
+        delete = findViewById(R.id.delete);
+        
     }
 }
